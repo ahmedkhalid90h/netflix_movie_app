54dca8539808b1c9887c897cf8f7b7419c09d01d
ahmedkhaled90h <ahmedkhaled90h@gmail.com>
2024-04-01 16:56:15 +0200

    part 2  scroll row between filmes

--- movie-app/src/components/list/List.jsx
+++ movie-app/src/components/list/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import ListItem from '../listitem/ListItem'
 import './list.css'
@@ -6,17 +6,33 @@ import './list.css'
 
 
 const List = () => {
+    const [isMoved, setIsMoved] =  useState(false)
+    const [slideNumber, setSlideNumber] =  useState(0)
+
+    const listRef =  useRef()
+
+    const handeleClick = (click) => {
+        setIsMoved(true)
+        let distance = listRef.current.getBoundingClientRect().x - 50
+        if (click === 'left' && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1)
+            listRef.current.style.transform = `translateX(${230 + distance}px)`
+        }
+        if (click === 'right' && slideNumber < 5) {
+            setSlideNumber(slideNumber + 1)
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`
+        }
+    }
     return (
-        <div className='list'>
+        <div className='list' >
             <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
-            <IoIosArrowBack className='sliderArrow left' />
-            <div className="container">
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
+            <IoIosArrowBack
+                className='sliderArrow left'
+                onClick={() => handeleClick('left')}
+                style={{display: !isMoved && 'none'}}
+            />
+            <div className="container" ref={listRef}>
                 <ListItem />
                 <ListItem />
                 <ListItem />
@@ -28,7 +44,7 @@ const List = () => {
                 <ListItem />
                 <ListItem />
             </div>
-            <IoIosArrowForward className='sliderArrow right' />
+            <IoIosArrowForward className='sliderArrow right' onClick={() => handeleClick('right')} />
             </div>
         </div>
     )

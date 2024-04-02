4f4de53ec6b7b82464e46a0746c0ba6811799e24
ahmedkhaled90h <ahmedkhaled90h@gmail.com>
2024-04-02 22:55:22 +0200

    add get router in file usre.js

--- api/router/users.js
+++ api/router/users.js
@@ -5,23 +5,7 @@ import verify  from '../verifyToken.js';
 const router = Router();
 
 
-
-//Update
-router.delete("/:id", verify, async (req,res) => {
-    if (req.user.id === req.params.id || req.user.isAdmin) {
-        try {
-            await User.findByIdAndDelete(req.params.id)
-            res.status(200).json('User has been deleted...')
-        } catch (err) {
-            res.status(500).json({ message: "Internal Server Error" });
-        }
-    } else {
-        res.status(403).json('You can delete only your account!')
-    }
-})
-
-
-// Delete
+// Update
 router.put("/:id", verify, async (req,res) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
         if (req.body.password) {
@@ -46,10 +30,34 @@ router.put("/:id", verify, async (req,res) => {
     }
 })
 
+// Delete
+router.delete("/:id", verify, async (req,res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json('User has been deleted...')
+        } catch (err) {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    } else {
+        res.status(403).json('You can delete only your account!')
+    }
+})
+
 
 // Get
+router.get("/find/:id", verify, async (req,res) => {
+        try {
+            const user = await User.findById(req.params.id)
+            const { password, ...info } = user._doc
+            res.status(200).json(info)
+        } catch (err) {
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+})
 
 // Get All
+
 
 // Get User Stats
 

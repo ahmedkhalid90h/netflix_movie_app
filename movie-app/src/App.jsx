7eb7cb1e985f1c12ee1610f163c06aa4fe589552
ahmedkhaled90h <ahmedkhaled90h@gmail.com>
2024-04-02 17:22:34 +0200

    add some css to register and coplete login page

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './pages/home/Home';
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 
 const App = () => {
